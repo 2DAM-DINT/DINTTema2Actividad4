fe8e3c075875e89794b7982db48367a8104ef3a2
adrianCG <adrianCG@mesi>
2020-10-24 11:00:28 +0200

    Agregar archivos de proyecto.

---
+++ Actividad4/MainWindow.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace Actividad4
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (cuadroTexto.Text.Length >= 140)
+                cuadroTexto.IsReadOnly = true;
+            contador.Text = cuadroTexto.Text.Length.ToString() + "/140";
+        }
+    }
+}
